d5ac7414c08a407c36b616badb92bcb8ffb4c359
yanqi8 <yan.qi@yale.edu>
2018-02-05 10:13:10 -0500

    CETable

--- scr/CEPlot.py
+++ scr/CEPlot.py
@@ -22,37 +22,40 @@ class CEA:
     def FindFrontier(self):
         # convert data to panda data frame
         # sort by cost, ascending
-        data = pd.DataFrame({'cost': self.cost, 'effect': self.effect,\
+        data = pd.DataFrame({'Expected Cost': self.cost, 'Expected Utility': self.effect,\
                              'dominated': self.dominated, 'color': ['red']*self.n})
-        data = data.sort_values('cost')
+        data = data.sort_values('Expected Cost')
+        # assign names to strategies, alphabeta may not enough
+        data['Strategy'] = range(1, self.n + 1)
 
         # apply criteria 1
         for i in range(self.n):
             # get the points that dominated by point i and change their status
-            data.loc[(data['cost'] > data['cost'][i]) & (data['effect'] < data['effect'][i]), 'dominated'] = True
+            data.loc[(data['Expected Cost'] > data['Expected Cost'][i])\
+                     & (data['Expected Utility'] < data['Expected Utility'][i]), 'dominated'] = True
 
         data.loc[data['dominated'] == True, 'color'] = 'blue'
 
         # apply criteria 2
         data2 = data.loc[data['dominated'] == False]
-        n2 = len(data2['cost'])
+        n2 = len(data2['Expected Cost'])
 
         for i in range(0, n2): # can't decide for first and last point
             for j in range(i+1, n2):
                 # according to row numbers
-                x1 = data2['effect'].iloc[i]
-                y1 = data2['cost'].iloc[i]
+                x1 = data2['Expected Utility'].iloc[i]
+                y1 = data2['Expected Cost'].iloc[i]
 
-                x2 = data2['effect'].iloc[j]
-                y2 = data2['cost'].iloc[j]
+                x2 = data2['Expected Utility'].iloc[j]
+                y2 = data2['Expected Cost'].iloc[j]
 
                 v1 = np.array([x2-x1, y2-y1])
 
-                if ((data2['effect'] > x1) & (data2['effect'] < x2)).any() == False: continue
+                if ((data2['Expected Utility'] > x1) & (data2['Expected Utility'] < x2)).any() == False: continue
                 else:
-                    inner_points = data2.loc[(data2['effect'] > x1) & (data2['effect'] < x2)]
-                    v2_x = inner_points['effect'] - x1
-                    v2_y = inner_points['cost']-y1
+                    inner_points = data2.loc[(data2['Expected Utility'] > x1) & (data2['Expected Utility'] < x2)]
+                    v2_x = inner_points['Expected Utility'] - x1
+                    v2_y = inner_points['Expected Cost']-y1
                     cross_product = v1[0] * np.array(v2_y) - v1[1] * np.array(v2_x)
                     # if cross_product > 0 the point is above the line
                     # (because the point are sorted vertically)
@@ -62,13 +65,13 @@ class CEA:
                     data.loc[list(ind_remove), 'color'] = 'blue'
 
         # plots
-        linedat = data.loc[data["dominated"] == False].sort_values('effect')
-        plt.scatter(data['effect'], data['cost'], c=list(data['color']), alpha=0.6)
-        plt.plot(linedat['effect'], linedat['cost'], c='red', alpha=0.6)
+        linedat = data.loc[data["dominated"] == False].sort_values('Expected Utility')
+        plt.scatter(data['Expected Utility'], data['Expected Cost'], c=list(data['color']), alpha=0.6)
+        plt.plot(linedat['Expected Utility'], linedat['Expected Cost'], c='red', alpha=0.6)
         plt.axhline(y=0, color='k')
         plt.axvline(x=0, color='k')
-        plt.xlabel('effect')
-        plt.ylabel('cost')
+        plt.xlabel('Expected Utility')
+        plt.ylabel('Expected Cost')
         plt.show()
 
         return data
@@ -76,13 +79,46 @@ class CEA:
 
 
     def BuildCETable(self):
-        pass
+        data = self.FindFrontier()
+        data['Expected Incremental Cost'] = "-"
+        data['Expected Incremental Utility'] = "-"
+        data['ICER'] = "Dominated"
+        not_dominated_points = data.loc[data["dominated"] == False].sort_values('Expected Cost')
+
+        n_not_dominated = not_dominated_points.shape[0]
+
+
+        incre_cost = []
+        incre_utility = []
+        ICER = []
+        for i in range(1, n_not_dominated):
+            temp_num = not_dominated_points["Expected Cost"].iloc[i]-not_dominated_points["Expected Cost"].iloc[i-1]
+            incre_cost = np.append(incre_cost, temp_num)
+
+            temp_den = not_dominated_points["Expected Utility"].iloc[i]-not_dominated_points["Expected Utility"].iloc[i-1]
+            if temp_den == 0:
+                raise ValueError('invalid value of Expected Incremental Utility, the ratio is not computable')
+            incre_utility = np.append(incre_utility, temp_den)
+
+            ICER = np.append(ICER, temp_num/temp_den)
+
+        ind_change = not_dominated_points.index[1:]
+        data.loc[ind_change, 'Expected Incremental Cost'] = incre_cost
+        data.loc[ind_change, 'Expected Incremental Utility'] = incre_utility
+        data.loc[ind_change, 'ICER'] = ICER
+        data.loc[not_dominated_points.index[0], 'ICER'] = '-'
+
+        return data[['Strategy', 'Expected Cost', 'Expected Utility', 'Expected Incremental Cost', 'Expected Incremental Utility',\
+            'ICER']]
+
+
+
 
 np.random.seed(573)
-cost = np.random.normal(0, 5, 50)
-effect = np.random.normal(0, 5, 50)
-
-s = Strategy(cost,effect)
+cost = np.random.normal(0, 5, 20)
+effect = np.random.normal(0, 5, 20)
+s = Strategy(cost,effect) # unsorted
 
 myCEA = CEA(s)
 data = myCEA.FindFrontier()
+table = myCEA.BuildCETable()
