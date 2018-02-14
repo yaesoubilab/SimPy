f97ab278752269826dd666b6f13af7cc74c681c7
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-02-13 22:13:05 -0500

    fixing histogram

--- scr/HistogramFunctions.py
+++ scr/HistogramFunctions.py
@@ -1,15 +1,9 @@
 from enum import Enum
 import matplotlib.pyplot as plt
+from scr import FigureSupport as Fig
 
 
-class OutType(Enum):
-    """output types for plotted figures"""
-    SHOW = 1    # show
-    JPG = 2     # save the figure as a jpg file
-    PDF = 3     # save the figure as a pdf file
-
-
-def graph_histogram(observations, title, x_label, y_label, output_type, legend=None):
+def graph_histogram(observations, title, x_label, y_label, output_type=Fig.OutType.SHOW, legend=None):
     """ graphs the histogram of observations
     :param observations: list of observations
     :param title: (string) title of the figure
@@ -34,20 +28,4 @@ def graph_histogram(observations, title, x_label, y_label, output_type, legend=N
         plt.legend([legend])
 
     # output figure
-    output_figure(plt, output_type, title)
-
-
-def output_figure(plt, output_type, title):
-    """
-    :param plt: reference to the plot
-    :param output_type: select from OutType.SHOW, OutType.PDF, or OutType.JPG
-    :param title: figure title
-    :return:
-    """
-    # output
-    if output_type == OutType.SHOW:
-        plt.show()
-    elif output_type == OutType.JPG:
-        plt.savefig(title+".png")
-    elif output_type == OutType.PDF:
-        plt.savefig(title+".pdf")
+    Fig.output_figure(plt, output_type, title)
--- tests/TestHistogram.py
+++ tests/TestHistogram.py
@@ -3,4 +3,9 @@ import scr.HistogramFunctions as cls
 
 
 obs = np.random.normal(10, 2, 1000)
-cls.graph_histogram(obs, 'Histogram', 'Values', 'Counts', cls.OutType.SHOW, 'Number of patients')
+cls.graph_histogram(
+    observations=obs,
+    title='Histogram',
+    x_label='Values',
+    y_label='Counts',
+    legend='Number of patients')
