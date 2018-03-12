7bb4a2698c6744d6f93ce79a5d421914a0d88dcc
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-03-12 16:30:33 -0400

    adding multiple histograms

--- tests/TestHistogram.py
+++ tests/TestHistogram.py
@@ -1,14 +1,14 @@
 import numpy as np
 import scr.FigureSupport as cls
 
-# obs = np.random.normal(4, 3, 1000)
-# cls.graph_histogram(
-#     data=obs,
-#     title='Histogram',
-#     x_label='Values',
-#     y_label='Counts',
-#     x_range=[-5,20],
-#     legend='Number of patients')
+obs = np.random.normal(4, 3, 1000)
+cls.graph_histogram(
+    data=obs,
+    title='Histogram',
+    x_label='Values',
+    y_label='Counts',
+    x_range=[-5,20],
+    legend='Number of patients')
 
 obs_sets = [
     np.random.normal(4, 3, 1000),
