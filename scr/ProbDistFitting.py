6afd65fbfc5dbd8a5043f92a2770b7cd6399aa15
yanqi8 <yan.qi@yale.edu>
2018-03-14 22:01:36 -0400

    #15 update MLE and AIC

--- scr/ProbDistFitting.py
+++ scr/ProbDistFitting.py
@@ -11,19 +11,9 @@ dat_gamma = np.random.gamma(10,2,1000)
 dat_poisson = np.random.poisson(3, 1000)
 
 def fit_poisson(data):
-    # fit poisson distribution
-    # the bins should be of integer width, because poisson is an integer distribution
-    entries, bin_edges, patches = plt.hist(data, bins=11, range=[-0.5, 10.5], normed=True)
-
-    # calculate bin middles
-    bin_middles = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-
-    # poisson function, parameter lamb is the fit parameter
-    def poisson(k, lamb):
-        return (lamb ** k / factorial(k)) * np.exp(-lamb)
-
-    # fit with curve_fit
-    lamb, cov_matrix = curve_fit(poisson, bin_middles, entries)
+    # fit poisson distribution: the MLE of lambda is the sample mean
+    # https://en.wikipedia.org/wiki/Poisson_distribution#Maximum_likelihood
+    lamb = data.mean()
 
     # plot histogram
     fig, ax = plt.subplots(1, 1)
@@ -38,8 +28,15 @@ def fit_poisson(data):
     # ax.set_xlabel("data")
     ax.set_ylabel("Frequency")
 
+    # calculate AIC
+    logLik = np.sum(scs.poisson.logpmf(data, lamb))
+    k = 1
+    aic = 2 * k - 2 * logLik
+
     print("Lambda:")
     print('Poisson', lamb)
+    print("AIC")
+    print(aic)
 
     ax.legend()
     plt.show()
@@ -59,12 +56,26 @@ def fit_gamma(data):
     # ax.set_xlabel("data")
     ax.set_ylabel("Frequency")
 
+    # calculate AIC
+    logLik = np.sum(scs.gamma.logpdf(data, a, loc, scale))
+    k = 3
+    aic = 2 * k - 2 * logLik
+
     print("a, loc, scale:")
     print('Gamma', a, loc, scale)
+    print("AIC")
+    print(aic)
 
     ax.legend()
     plt.show()
 
 
+# for simulated data from Gamma distribution,
+# AIC for gamma is smaller
 fit_gamma(dat_gamma)
+fit_poisson(dat_gamma) # Inf since continuous
+
+# for simulated data from Poisson distribution,
+# AIC for Poisson is smaller
 fit_poisson(dat_poisson)
+fit_gamma(dat_poisson)
