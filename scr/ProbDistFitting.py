44a8be97fe870649ccdea0d33a47961151c9cd03
yanqi8 <yan.qi@yale.edu>
2018-03-18 16:55:30 -0400

    #19 fit distributions

--- scr/ProbDistFitting.py
+++ scr/ProbDistFitting.py
@@ -5,30 +5,73 @@ import scipy.stats as scs
 COLOR_CONTINUOUS_FIT = 'r'
 COLOR_DISCRETE_FIT = 'r'
 
+# In most distributions with Location and Scale parameters, floc=0 is applied
+# (fix location at 0), since if not, estimated parameters are not unique
 
 def AIC(k, log_likelihood):
     """ :returns Akaike information criterion"""
     return 2 * k - 2 * log_likelihood
 
+# 1 Exponential
+def fit_exp(data, x_label):
+    """
+    :param data: (numpy.array) observations
+    :param x_label: label to show on the x-axis of the histogram
+    :returns: dictionary with keys "loc", "scale", and "AIC"
+    """
 
+    # plot histogram
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, normed=1, bins='auto', edgecolor='black', alpha=0.5, label='Frequency')
+
+    # estimate the parameters of exponential
+    loc, scale = scs.expon.fit(data, floc=0)
+
+    # plot the estimated exponential distribution
+    x_values = np.linspace(scs.expon.ppf(0.0001, loc, scale), scs.expon.ppf(0.9999, loc, scale), 200)
+    rv = scs.expon(loc, scale)
+    ax.plot(x_values, rv.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='Exponential')
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel("Frequency")
+    ax.legend()
+    plt.show()
+
+    # calculate AIC
+    aic = AIC(
+        k=1, # lambda = 1/scale
+        log_likelihood=np.sum(scs.expon.logpdf(data, loc, scale))
+    )
+
+    # report results in the form of a dictionary
+    return {"loc": loc, "scale": scale, "AIC": aic}
+
+# 2 Beta
+
+# 3 BetaBinomial
+# 4 Binomial
+# 5 Empirical (I guess for this, we just need to return the frequency of each observation)
+
+
+# 6 Gamma
 def fit_gamma(data, x_label):
     """
     :param data: (numpy.array) observations
     :param x_label: label to show on the x-axis of the histogram
     :returns: dictionary with keys "a", "loc", "scale", and "AIC"
     """
-    
+
     # plot histogram
     fig, ax = plt.subplots(1, 1)
     ax.hist(data, normed=1, bins='auto', edgecolor='black', alpha=0.5, label='Frequency')
 
     # estimate the parameters of gamma
     a, loc, scale = scs.gamma.fit(data)
-    
-    # plot the estimated gamma distribution 
+
+    # plot the estimated gamma distribution
     x_values = np.linspace(scs.gamma.ppf(0.0001, a, loc, scale), scs.gamma.ppf(0.9999, a, loc, scale), 200)
-    gamma_dist = scs.gamma(a, loc, scale)
-    ax.plot(x_values, gamma_dist.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='Gamma')
+    rv = scs.gamma(a, loc, scale)
+    ax.plot(x_values, rv.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='Gamma')
 
     ax.set_xlabel(x_label)
     ax.set_ylabel("Frequency")
@@ -45,6 +88,124 @@ def fit_gamma(data, x_label):
     return {"a": a, "loc": loc, "scale": scale, "AIC": aic}
 
 
+# 7 GammaPoisson
+# 8 Geometric
+
+# 9 JohnsonSb
+def fit_johnsonSb(data, x_label):
+    """
+    :param data: (numpy.array) observations
+    :param x_label: label to show on the x-axis of the histogram
+    :returns: dictionary with keys "a", "b", "loc", "scale", and "AIC"
+    """
+
+    # plot histogram
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, normed=1, bins='auto', edgecolor='black', alpha=0.5, label='Frequency')
+
+    # estimate the parameters
+    a, b, loc, scale = scs.johnsonsb.fit(data, floc=0)
+
+    # plot the estimated JohnsonSb distribution
+    x_values = np.linspace(scs.johnsonsb.ppf(0.01, a, b, loc, scale),
+                           scs.johnsonsb.ppf(0.99, a, b, loc, scale), 100)
+    rv = scs.johnsonsb(a, b, loc, scale)
+    ax.plot(x_values, rv.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='JohnsonSb')
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel("Frequency")
+    ax.legend()
+    plt.show()
+
+    # calculate AIC
+    aic = AIC(
+        k=3, # loc is fixed at 0, so 3 parameters
+        log_likelihood=np.sum(scs.johnsonsb.logpdf(data, a, b, loc, scale))
+    )
+
+    # report results in the form of a dictionary
+    return {"a": a, "b": b, "loc": loc, "scale": scale, "AIC": aic}
+
+# 10 JohnsonSu
+def fit_johnsonSu(data, x_label):
+    """
+    :param data: (numpy.array) observations
+    :param x_label: label to show on the x-axis of the histogram
+    :returns: dictionary with keys "a", "b", "loc", "scale", and "AIC"
+    """
+
+    # plot histogram
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, normed=1, bins='auto', edgecolor='black', alpha=0.5, label='Frequency')
+
+    # estimate the parameters
+    a, b, loc, scale = scs.johnsonsu.fit(data, floc=0)
+
+    # plot the estimated JohnsonSu distribution
+    x_values = np.linspace(scs.johnsonsu.ppf(0.01, a, b, loc, scale),
+                           scs.johnsonsu.ppf(0.99, a, b, loc, scale), 100)
+    rv = scs.johnsonsu(a, b, loc, scale)
+    ax.plot(x_values, rv.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='JohnsonSu')
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel("Frequency")
+    ax.legend()
+    plt.show()
+
+    # calculate AIC
+    aic = AIC(
+        k=3, # loc is fixed at 0, so 3 parameters
+        log_likelihood=np.sum(scs.johnsonsu.logpdf(data, a, b, loc, scale))
+    )
+
+    # report results in the form of a dictionary
+    return {"a": a, "b": b, "loc": loc, "scale": scale, "AIC": aic}
+
+
+
+# 11 LogNormal
+# 12 NegativeBinomial
+# 13 Normal
+# 14 Triangular
+# 15 Uniform
+# 16 UniformDiscrete
+
+# 17 Weibull
+def fit_weibull(data, x_label):
+    """
+    :param data: (numpy.array) observations
+    :param x_label: label to show on the x-axis of the histogram
+    :returns: dictionary with keys "a", "loc", "scale", and "AIC"
+    """
+
+    # plot histogram
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(data, normed=1, bins='auto', edgecolor='black', alpha=0.5, label='Frequency')
+
+    # estimate the parameters of weibull
+    # location is fixed at 0
+    a, loc, scale = scs.weibull_min.fit(data, floc=0)
+
+    # plot the estimated gamma distribution
+    x_values = np.linspace(scs.weibull_min.ppf(0.01, a, loc, scale), scs.weibull_min.ppf(0.99, a, loc, scale), 100)
+    rv = scs.weibull_min(a, loc, scale)
+    ax.plot(x_values, rv.pdf(x_values), color=COLOR_CONTINUOUS_FIT, lw=2, label='Weibull')
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel("Frequency")
+    ax.legend()
+    plt.show()
+
+    # calculate AIC
+    aic = AIC(
+        k=2, # as location is fixed, only 2 parameters estimated
+        log_likelihood=np.sum(scs.weibull_min.logpdf(data, a, loc, scale))
+    )
+
+    # report results in the form of a dictionary
+    return {"a": a, "loc": loc, "scale": scale, "AIC": aic}
+
+# 18 Poisson
 def fit_poisson(data, x_label):
     """
     :param data: (numpy.array) observations
--- tests/TestProbDistFitting.py
+++ tests/TestProbDistFitting.py
@@ -1,15 +1,42 @@
 import scr.ProbDistFitting as Fit
 import numpy as np
+import scr.RandomVariantGenerators as RVGs
+import scipy.stats as scs
 
 # simulate some data
 np.random.seed(1)
 
-# fitting a gamma distribution
+# 1 fitting a exponential distribution
+dat_exp = np.random.exponential(5, 1000)            # generate data
+dictResults=Fit.fit_exp(dat_exp, 'Data')        # fit
+print("Fitting Exponential:", dictResults)
+
+
+
+
+
+
+# 6 fitting a gamma distribution
 dat_gamma = np.random.gamma(10, 2,1000)     # generate data
 dictResults=Fit.fit_gamma(dat_gamma, 'Data')        # fit
 print("Fitting Gamma:", dictResults)
 
-# fitting a Poisson distribution
+# 9 fitting a JohnsonSb distribution
+dat_JohnsonSb = scs.johnsonsb.rvs(a=10, b=3, loc=0, scale=1, size=1000)    # generate data
+dictResults=Fit.fit_johnsonSb(dat_JohnsonSb, 'Data')    # fit
+print("Fitting johnsonSb:", dictResults)
+
+# 10 fitting a JohnsonSu distribution
+dat_JohnsonSu = scs.johnsonsu.rvs(a=10, b=3, loc=0, scale=1, size=1000)    # generate data
+dictResults=Fit.fit_johnsonSu(dat_JohnsonSu, 'Data')    # fit
+print("Fitting johnsonSu:", dictResults)
+
+# 17 fitting a Weibull distribution
+dat_weibull = np.random.weibull(5, 1000)    # generate data
+dictResults=Fit.fit_weibull(dat_weibull, 'Data')    # fit
+print("Fitting Weibull:", dictResults)
+
+# 18 fitting a Poisson distribution
 dat_poisson = np.random.poisson(30, 1000)    # generate data
 dictResults=Fit.fit_poisson(dat_poisson, 'Data')    # fit
 print("Fitting Poisson:", dictResults)
