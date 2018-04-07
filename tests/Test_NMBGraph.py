ac5f55f5b54d67e87bfa132c067bdcee7b0f10d4
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-04-06 21:18:45 -0400

    CBA

--- tests/Test_NMBGraph.py
+++ tests/Test_NMBGraph.py
@@ -17,21 +17,21 @@ nmb_indp = ce.CBA([s0, s1, s2], if_paired=False) # list of frontier strategies a
 # Try NMB_Lines figure - paired CI
 nmb_paired.graph_deltaNMB_lines(0,50000,"deltaNMB lines for paired CI","wtp values","NMB values",
                                 interval=ce.Interval.CONFIDENCE,
-                                show_legend=True, figure_size=8)
+                                show_legend=True, figure_size=6)
 
 # Try NMB_Lines figure - paired PI
 nmb_paired.graph_deltaNMB_lines(0,50000,"deltaNMB lines for paired PI","wtp values","NMB values",
                                 interval=ce.Interval.PREDICTION,
-                                show_legend=True, figure_size=8)
+                                show_legend=True, figure_size=6)
 
 # Try NMB_Lines figure - indp CI
 nmb_indp.graph_deltaNMB_lines(0,50000,"deltaNMB lines for indp CI","wtp values","NMB values",
                               interval=ce.Interval.CONFIDENCE,
-                              show_legend=True, figure_size=8)
+                              show_legend=True, figure_size=6)
 
 # Try NMB_Lines figure - indp PI
 nmb_indp.graph_deltaNMB_lines(0,50000,"deltaNMB lines for indp PI","wtp values","NMB values",
                               interval=ce.Interval.PREDICTION,
-                              show_legend=True, figure_size=8)
+                              show_legend=True, figure_size=6)
 
 
