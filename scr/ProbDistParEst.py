3b809c1527b18657f7633ff7c919b5dc7e6f5343
yanqi8 <yan.qi@yale.edu>
2018-04-16 14:53:14 -0400

    #19 moment matching

--- scr/ProbDistParEst.py
+++ scr/ProbDistParEst.py
@@ -10,55 +10,33 @@ warnings.filterwarnings("ignore")
 COLOR_CONTINUOUS_FIT = 'r'
 COLOR_DISCRETE_FIT = 'r'
 
-def get_gamma_parameters(mean, st_dev):
-    """
-    :param mean: sample mean of an observation set
-    :param st_dev: sample standard deviation of an observation set
-    :return: shape and scale of the gamma distribution with mean and standard deviation matching the
-    provided sample mean and sample st_dev
-    """
-
-    shape = (mean / st_dev) ** 2
-    scale = (st_dev ** 2) / mean
-    
-    return shape, scale
 
 
-def get_beta_parameters(mean, st_dev):
+
+
+
+
+
+
+# 1 Exponential
+
+
+# 2 Beta
+def get_beta_parameters(mean, st_dev, min=0, max=1):
     """
     :param mean: sample mean of an observation set
     :param st_dev: sample standard deviation of an observation set
     :return: alpha and beta of the gamma distribution with mean and standard deviation matching the
     provided sample mean and sample st_dev
     """
+    # shift the distribution by loc and scale
+    mean = mean-min
+    st_dev = st_dev/(max-min)*1.0
 
     aPlusB = mean*(1-mean)/st_dev**2 - 1
     a = mean*aPlusB
 
-    return a, aPlusB - a
-
-
-def get_log_normal_parameters(mean, st_dev):
-    """
-    :param mean: sample mean of an observation set
-    :param st_dev: sample standard deviation of an observation set
-    :return: mu and sigma the lognormal distribution with mean and standard deviation matching the
-    provided sample mean and sample st_dev
-    """
-
-    mu = math.log(
-        mean**2 / math.sqrt(st_dev**2 + mean**2)
-    )
-    sigma = math.sqrt(
-        math.log(1 + st_dev**2 / mean**2)
-    )
-
-    return mu, sigma
-
-# 1 Exponential
-
-
-# 2 Beta
+    return {"a": a, "b": aPlusB - a, "loc": min, "scale": max-min}
 
 
 # 3 BetaBinomial
@@ -95,6 +73,21 @@ def fit_empirical(data, x_label):
 
 
 # 6 Gamma
+def get_gamma_parameters(mean, st_dev, fixed_location=0):
+    """
+    :param mean: sample mean of an observation set
+    :param st_dev: sample standard deviation of an observation set
+    :return: shape and scale of the gamma distribution with mean and standard deviation matching the
+    provided sample mean and sample st_dev
+    """
+    mean = mean - fixed_location
+
+    shape = (mean / st_dev) ** 2
+    scale = (st_dev ** 2) / mean
+
+    # report results in the form of a dictionary
+    return {"a": shape, "loc": fixed_location, "scale": scale}
+
 
 
 # 7 GammaPoisson
@@ -110,6 +103,24 @@ def fit_empirical(data, x_label):
 
 
 # 11 LogNormal
+def get_log_normal_parameters(mean, st_dev, fixed_location=0):
+    """
+    :param mean: sample mean of an observation set
+    :param st_dev: sample standard deviation of an observation set
+    :return: mu and sigma the lognormal distribution with mean and standard deviation matching the
+    provided sample mean and sample st_dev
+    """
+
+    mean = mean-fixed_location
+
+    mu = np.log(
+        mean**2 / np.sqrt(st_dev**2 + mean**2)
+    )
+    sigma = np.sqrt(
+        np.log(1 + st_dev**2 / mean**2)
+    )
+
+    return {"s": sigma, "loc": fixed_location, "scale": np.exp(mu)}
 
 
 # 12 NegativeBinomial
