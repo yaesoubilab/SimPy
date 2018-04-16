4aabb76418acde302cbe45baf255904acef60e5f
yanqi8 <yan.qi@yale.edu>
2018-04-16 16:05:35 -0400

    #19 moment matching

--- scr/ProbDistParEst.py
+++ scr/ProbDistParEst.py
@@ -1,8 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.stats as scs
 import scipy as sp
-from scipy.optimize import fmin_slsqp
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -12,17 +10,20 @@ COLOR_DISCRETE_FIT = 'r'
 
 
 
-
-
-
-
-
-
 # 1 Exponential
+def get_expon_params(mean, fixed_location=0):
+    """
+    :param mean: sample mean of an observation set
+    :param fixed_location: specify location, 0 by default
+    :return: scale and location
+    """
+    mean = mean - fixed_location
+    scale = mean
 
+    return {"loc": fixed_location, "scale": scale}
 
 # 2 Beta
-def get_beta_parameters(mean, st_dev, min=0, max=1):
+def get_beta_params(mean, st_dev, min=0, max=1):
     """
     :param mean: sample mean of an observation set
     :param st_dev: sample standard deviation of an observation set
@@ -43,10 +44,20 @@ def get_beta_parameters(mean, st_dev, min=0, max=1):
 
 
 # 4 Binomial
+def get_binomial_parameters(mean, st_dev, fixed_location=0):
+    """
+    :param mean: sample mean of an observation set
+    :param st_dev: sample standard deviation of an observation set
+    :param fixed_location: specify location, 0 by default
+    :return: success probability p and location
+    """
+    mean = mean-fixed_location
+    p = 1.0 - (st_dev**2)/mean
 
+    return {"p": p, "loc": fixed_location}
 
 # 5 Empirical (I guess for this, we just need to return the frequency of each observation)
-def fit_empirical(data, x_label):
+def get_empirical_parameters(data, x_label):
     """
     :param data: (numpy.array) observations
     :param x_label: label to show on the x-axis of the histogram
@@ -55,28 +66,14 @@ def fit_empirical(data, x_label):
     unique, counts = np.unique(data, return_counts=True)
     freq = counts*1.0/len(data)
 
-    # plot histogram
-    fig, ax = plt.subplots(1, 1)
-    ax.hist(data, normed=1, bins=np.max(data)+1, range=[-0.5, np.max(data)+0.5],
-            edgecolor='black', alpha=0.5, label='Frequency')
-
-    # plot with fitted parameter
-    x_plot = unique
-    ax.step(unique, freq, COLOR_DISCRETE_FIT, ms=8, label='Empirical')
-
-    ax.set_xlabel(x_label)
-    ax.set_ylabel("Frequency")
-    ax.legend()
-    plt.show()
-
     return unique,freq
-
 
 # 6 Gamma
 def get_gamma_parameters(mean, st_dev, fixed_location=0):
     """
     :param mean: sample mean of an observation set
     :param st_dev: sample standard deviation of an observation set
+    :param fixed_location: specify location, 0 by default
     :return: shape and scale of the gamma distribution with mean and standard deviation matching the
     provided sample mean and sample st_dev
     """
@@ -94,10 +91,20 @@ def get_gamma_parameters(mean, st_dev, fixed_location=0):
 
 
 # 8 Geometric
+def get_geomertic_paras(mean, fixed_location=0):
+    """
+    :param mean: sample mean of an observation set
+    :param fixed_location:
+    :return: probability p and location
+    """
+    mean = mean - fixed_location
+    p = 1.0/mean
+
+    return {"p": p, "loc": fixed_location}
 
 
 # 9 JohnsonSb
-
+# too many parameters ??
 
 # 10 JohnsonSu
 
@@ -127,6 +134,15 @@ def get_log_normal_parameters(mean, st_dev, fixed_location=0):
 
 
 # 13 Normal
+def get_normal_param(mean, st_dev):
+    """
+    :param mean: sample mean of an observation set
+    :param st_dev: sample standard deviation of an observation set
+    :param fixed_location: specify location, 0 by default
+    :return: location and scale of the normal distribution
+    """
+
+    return {"loc": mean, "scale": st_dev}
 
 
 # 14 Triangular
--- tests/TestProbDistParEst.py
+++ tests/TestProbDistParEst.py
@@ -8,47 +8,47 @@ import scipy.stats as scs
 np.random.seed(1)
 
 # 1 fitting a exponential distribution
-# dat_exp = np.random.exponential(5, 1000)            # generate data
-# dictResults=Est.fit_exp(dat_exp, 'Data')        # fit
-# print("Fitting Exponential:", dictResults)
-#
+dat_exp = np.random.exponential(5, 1000)            # generate data
+dictResults=Est.get_expon_params(np.mean(dat_exp))        # fit
+print("Fitting Exponential:", dictResults)
+
 # 2 fitting a beta distribution
 dat_beta = np.random.beta(2, 3, 1000)            # generate data
-dictResults=Est.get_beta_parameters(np.mean(dat_beta), np.std(dat_beta)) # fit
+dictResults=Est.get_beta_params(np.mean(dat_beta), np.std(dat_beta)) # fit
 print("Fitting Beta:", dictResults)
-#
-# # 3 fitting a beta-binomial distribution
+
+# 3 fitting a beta-binomial distribution
 # betabinomial_dist = RVGs.BetaBinomial(100, 2, 3) # n, a, b
 # dat_betabin = np.array(get_samples(betabinomial_dist, np.random))
 # dictResults=Est.fit_betaBinomial(dat_betabin, 'Data', n=100) # fit
 # print("Fitting BetaBinomial:", dictResults)
-#
-# # 4 Binomial
-# dat_bin = np.random.binomial(100, 0.3, 1000)
-# dictResults=Est.fit_binomial(dat_bin, 'Data', n=100, fixed_location=0) # fit
-# print("Fitting Binomial:", dictResults)
-#
+
+# 4 Binomial
+dat_bin = np.random.binomial(100, 0.3, 1000)
+dictResults=Est.get_binomial_parameters(np.mean(dat_bin), np.std(dat_bin), fixed_location=0) # fit
+print("Fitting Binomial:", dictResults)
+
 # 5 Empirical (for int data)
 dat_em = np.random.poisson(30, 1000)
-dictResults=Est.fit_empirical(dat_em, 'Data') # fit
+dictResults=Est.get_empirical_parameters(dat_em, 'Data') # fit
 print("Fitting Empirical:", dictResults)
 
 # 6 fitting a Gamma distribution
 dat_gamma = np.random.gamma(10, 2, 1000)     # generate data
 dictResults=Est.get_gamma_parameters(np.mean(dat_gamma), np.std(dat_gamma))        # fit
 print("Fitting Gamma:", dictResults)
-#
-# # 7 GammaPoisson
+
+# 7 GammaPoisson
 # gamma_poisson_dist = RVGs.GammaPoisson(a=2, gamma_scale=4)
 # dat_gamma_poisson = np.array(get_samples(gamma_poisson_dist, np.random))
 # dictResults=Est.fit_GammaPoisson(dat_gamma_poisson, 'Data') # fit
 # print("Fitting GammaPoisson:", dictResults)
-#
-# # 8 Geometric
-# dat_geom = np.random.geometric(0.3, 1000)     # generate data
-# dictResults=Est.fit_geometric(dat_geom, 'Data', fixed_location=0)        # fit
-# print("Fitting Geometric:", dictResults)
-#
+
+# 8 Geometric
+dat_geom = np.random.geometric(0.3, 1000)     # generate data
+dictResults=Est.get_geomertic_paras(np.mean(dat_geom), fixed_location=0)        # fit
+print("Fitting Geometric:", dictResults)
+
 # # 9 fitting a JohnsonSb distribution
 # dat_JohnsonSb = scs.johnsonsb.rvs(a=10, b=3, loc=0, scale=1, size=1000)    # generate data
 # dictResults=Est.fit_johnsonSb(dat_JohnsonSb, 'Data')    # fit
@@ -69,11 +69,10 @@ print("Fitting LogNormal:", dictResults)
 # dictResults=Est.fit_NegativeBinomial(dat_neg_bin, 'Data')
 # print("Fitting NegativeBinomial:", dictResults)
 #
-# # 13 Normal
-# dat_norm = np.random.normal(0, 1, 1000)    # mean, sigma
-# dictResults=Est.fit_norm(dat_norm, 'Data')    # fit
-# print("Fitting Normal:", dictResults)
-#
+# 13 Normal
+dictResults=Est.get_normal_param(5, 2)    # fit
+print("Fitting Normal:", dictResults)
+
 # # 14 Triangular
 # dat_tri = scs.triang.rvs(c=0.5, loc=0, scale=1, size=1000)
 # dictResults=Est.fit_triang(dat_tri, 'Data')    # fit
