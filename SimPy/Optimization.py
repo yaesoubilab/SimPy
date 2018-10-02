ae9a87bea99c8d48a5c44c7b2f7c91ad0ff1e85c
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-02 09:52:41 -0400

    minor edits to the optimization

--- SimPy/Optimization.py
+++ SimPy/Optimization.py
@@ -24,38 +24,35 @@ class StepSize:
 class StochasticApproximation:
     # stochastic approximation algorithm
 
-    def __init__(self, sim_model, step_size, derivative_step):
+    def __init__(self, sim_model, derivative_step, step_size=StepSize(a=1)):
         """
         :param sim_model: the simulation model to optimize
-        :param step_size: the step size rule
         :param derivative_step: derivative step if calculating slopes
+        :param step_size: the step size rule
         """
         self._simModel = sim_model
         self._stepSize = step_size
         self._derivativeStep = derivative_step
-        self._xStar = None
-        self._fStar = None
+        self.xStar = None   # optimal x value
+        self.fStar = None   # f(xStar)
 
-        self._is = []
-        self._xs = []
-        self._fs = []
+        self.itr_i = []     # iteration indices
+        self.itr_x = []     # x values over iterations
+        self.itr_f = []     # f values over iterations
         self._deltax=[]
 
     def minimize(self, max_itr, x0):
         """
         :param max_itr: maximum iteration to terminate the algorithm
-        :param x0: starting point
-        :return:
+        :param x0: (numpy.array) starting point
         """
-        x=x0
+
+        x = x0
         f = self._simModel.get_obj_value(x)
 
-        #i: number of itteration
-        self._is.append(0)
-        #x: independent variable
-        self._xs.append(x)
-        #f: value of the function given x
-        self._fs.append(f)
+        self.itr_i.append(0)
+        self.itr_x.append(x)
+        self.itr_f.append(f)
 
         #generate arrays to calculate derivative
         for k in range(0, len(x)):
@@ -65,28 +62,31 @@ class StochasticApproximation:
 
 
         for i in range(1, max_itr):
+
             # estimate the derivative at x and y
             derivativevector=np.array([])
             for j in range(0,len(x)):
                derivative=(self._simModel.get_obj_value(x+self._deltax[j])-f)/self._derivativeStep
                derivativevector=np.append(derivativevector,[derivative])
+
             # find a new x
             x = x - self._stepSize.get_value(i)*derivativevector
+
             # evaluate the model at x
             f = self._simModel.get_obj_value(x)
 
-            self._is.append(i)
-            self._xs.append(x)
-            self._fs.append(f)
+            self.itr_i.append(i)
+            self.itr_x.append(x)
+            self.itr_f.append(f)
 
         # store the optimal x and optimal objective value
-        self._xStar = x
-        self._fStar = f
+        self.xStar = x
+        self.fStar = f
 
     def plot_fs(self, fStar=None):
 
         fig, ax = plt.subplots(figsize=(6, 5))
-        ax.plot(self._is, self._fs)
+        ax.plot(self.itr_i, self.itr_f)
 
         plt.xlabel('Iteration')
         plt.ylabel('Objective Function')
@@ -97,7 +97,7 @@ class StochasticApproximation:
     def plot_xs(self, xStar=None):
 
         fig, ax = plt.subplots(figsize=(6, 5))
-        ax.plot(self._is, self._xs)
+        ax.plot(self.itr_i, self.itr_x)
 
         plt.xlabel('Iteration')
         plt.ylabel('x')
--- tests/Optimization/TestStochApproximation.py
+++ tests/Optimization/TestStochApproximation.py
@@ -4,15 +4,15 @@ import numpy as np
 
 # create an object for the stochastic approximation method
 mySimOpt = Opt.StochasticApproximation(
-    sim_model=ToyModels.Xto2(err_sigma=1),
+    sim_model=ToyModels.Xto2(err_sigma=10),
     step_size=Opt.StepSize(a=1),
     derivative_step=0.1)
 
 # find the minimum
-mySimOpt.minimize(max_itr=1000, x0=np.array([5,5]))
+mySimOpt.minimize(max_itr=1000, x0=np.array([-50,5]))
 
 # plot x and objective function values
 mySimOpt.plot_fs(fStar=0)
 mySimOpt.plot_xs(xStar=0)
 
-print(mySimOpt._xs)
+print(mySimOpt.itr_x)
