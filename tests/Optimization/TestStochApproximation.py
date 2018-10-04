dddd4107866cf041dad2b614e312b31130fc7520
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-10-03 21:35:22 -0400

    normalize derivative in optimization

--- tests/Optimization/TestStochApproximation.py
+++ tests/Optimization/TestStochApproximation.py
@@ -6,10 +6,10 @@ import numpy as np
 mySimOpt = Opt.StochasticApproximation(
     sim_model=ToyModels.Xto2(err_sigma=10),
     derivative_step=1,
-    step_size=Opt.StepSize(a=25))
+    step_size=Opt.StepSize(a=100))
 
 # find the minimum
-mySimOpt.minimize(max_itr=1000, x0=np.array([-50, 5]))
+mySimOpt.minimize(max_itr=5000, x0=np.array([-50, 5]))
 
 # plot x and objective function values
 mySimOpt.plot_f_itr(f_star=0)
