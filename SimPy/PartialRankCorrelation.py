be503db720f52c5631d24805e4c69880b9f0ebbf
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-01-08 23:13:22 -0500

    adding partial rank correlation

---
+++ SimPy/PartialRankCorrelation.py
@@ -0,0 +1,29 @@
+from scipy.stats import spearmanr
+import SimPy.FormatFunctions as F
+import SimPy.InOutFunctions as IO
+
+
+class PartialRankCorrelation:
+    def __init__(self, parameter_values, output_values):
+        """
+        :param parameter_values: (dictionary) of parameter values with parameter names as the key
+        :param output_values: (list) of output values
+        """
+
+        self.results = []  # each row [parameter name, correlation, p-value]
+
+        for paramName, paramValues in parameter_values.items():
+            # calculate Spearman rank-order correlation coefficient
+            coef, p = spearmanr(paramValues, output_values)
+            self.results.append([paramName, coef, p])
+
+    def export_to_csv(self, file_name='PartialRank.csv', decimal=3):
+
+        formatedResults = [['Parameter', 'Coefficient', 'P-Value']]
+        for row in self.results:
+            name = row[0]
+            coef = F.format_number(number=row[1], deci=decimal)
+            p = F.format_number(number=row[2], deci=decimal)
+            formatedResults.append([name, coef, p])
+
+        IO.write_csv(file_name=file_name, rows=formatedResults)
---
+++ tests/Statistics/TestPartialRankCorrelation.py
@@ -0,0 +1,23 @@
+import SimPy.PartialRankCorrelation as PRC
+from numpy.random import rand
+
+# prepare data
+param1 = rand(100) * 20
+param2 = rand(100) * -1
+output = param1 + 2*param2 + rand(100) * 10
+
+parameterValues = {'Par1': param1, 'Par2': param2}
+
+prc = PRC.PartialRankCorrelation(
+    parameter_values=parameterValues,
+    output_values=output)
+
+print(prc.results)
+prc.export_to_csv(decimal=4)
+
+# # interpret the significance
+# alpha = 0.05
+# if p > alpha:
+#     print('Samples are uncorrelated (fail to reject H0) p=%.3f' % p)
+# else:
+#     print('Samples are correlated (reject H0) p=%.3f' % p)
