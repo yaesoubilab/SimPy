f127acf04523efa7232dbbaa5236df3fb6680f53
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-02-28 10:44:53 -0500

    adding moving average

--- SimPy/FigureSupport.py
+++ SimPy/FigureSupport.py
@@ -177,3 +177,38 @@ def graph_histograms(data_sets, title, x_label, y_label,
 
     # output figure
     output_figure(plt, output_type, title)
+
+
+def get_moving_average(data, window=2):
+    """
+    calculates the moving average of a time-series
+    :param data: list of observations
+    :param window: the window (number of data points) over which the average should be calculated
+    :return: list of moving averages
+    """
+
+    if window < 2:
+        raise ValueError('The window over which the moving averages '
+                         'should be calculated should be greater than 1.')
+    if window >= len(data):
+        raise ValueError('The window over which the moving averages '
+                         'should be calculated should be less than the number of data points.')
+
+    averages = []
+
+    # the 'window - 1' averages cannot be calculated
+    for i in range(window-1):
+        averages.append(None)
+
+    # calculate the first moving average
+    moving_ave = sum(data[0:window])/window
+    averages.append(moving_ave)
+
+    for i in range(window, len(data)):
+        moving_ave = (moving_ave*window - data[i-window] + data[i])/window
+        averages.append(moving_ave)
+
+    return averages
+
+
+
---
+++ tests/InputOutput/TestMovingAverage.py
@@ -0,0 +1,17 @@
+import SimPy.FigureSupport as Fig
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = [1, 2, 3, 4, 5]
+print(Fig.get_moving_average(data=data, window=3))
+
+
+data = np.random.randn(100)
+
+fig = plt.figure(figsize=(4,2))
+ax = fig.add_subplot(111)
+ax.plot(range(100), data, alpha=0.5)
+ax.plot(range(100), Fig.get_moving_average(data, window=10), alpha=1)
+
+plt.show()
