8e53ea83da580022b29c7439145e40d7f286bc9d
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-05-30 07:53:46 -0400

    pyramid figure

--- SimPy/Plots/PopulationPyramids.py
+++ SimPy/Plots/PopulationPyramids.py
@@ -6,8 +6,9 @@ def plot_pyramids(observed_data, simulated_data,
                   title=None, fig_size=None,
                   x_lim=100,
                   y_labels=None,
-                  age_group_length=5,
-                  colors=('brown', 'grey'),
+                  age_group_width=5,
+                  colors=('brown', 'grey', 'blue'),
+                  legend=('Data', 'Model'),
                   transparency=0.4):
     """
     :param observed_data: example:
@@ -21,8 +22,10 @@ def plot_pyramids(observed_data, simulated_data,
     :param fig_size: (tuple) figure size (e.g. (2, 4))
     :param x_lim: maximum of x-axis (between 0 and 100)
     :param y_labels: (list) of strings for labels of y_axis
-    :param age_group_length: length of each age group in years.
-    :param colors: (tuple) example: ('brown', 'grey')
+    :param age_group_width: width of each age group in years.
+    :param colors: (tuple) example: ('brown', 'grey', 'blue')
+                                    for men, women and simulation bars
+    :param legend: (tuple) default ('Data', 'Model'). legend will not display if set to None
     :param transparency: transparency of bars
     """
 
@@ -51,10 +54,10 @@ def plot_pyramids(observed_data, simulated_data,
         raise ValueError('Male and female age groups should be the same.')
 
     # find maximum value of the y_axis
-    y_lim = m_ages[-1] + age_group_length/2
+    y_lim = m_ages[-1] + age_group_width / 2
 
     # either sets to default ticks or user input
-    y_ticks_places = range(0, len(m_ages) * age_group_length, age_group_length)
+    y_ticks_places = range(0, len(m_ages) * age_group_width, age_group_width)
 
     # either sets to default tick labels or user input
     if y_labels is None:
@@ -72,16 +75,18 @@ def plot_pyramids(observed_data, simulated_data,
     # ranges of x- and y-axes
     for i in [0, 1]:
         axis[i].xmin, axis[i].xmax, axis[i].ymin, axis[i].ymax \
-            = axis[i].axis([0, x_lim, -age_group_length*(1/2+1/10), y_lim+age_group_length*1/10])
+            = axis[i].axis([0, x_lim, -age_group_width * (1 / 2 + 1 / 10), y_lim + age_group_width * 1 / 10])
 
     # bar plot for males
-    axis[0].barh(m_ages, m_sizes, age_group_length,
-                 align='center', color=colors[0], edgecolor='black', alpha=transparency, label='Data')
+    axis[0].barh(m_ages, m_sizes, age_group_width,
+                 align='center', color=colors[0],
+                 edgecolor='black', alpha=transparency, label='Data')
     axis[0].invert_xaxis()
 
     # bar plot for females
-    axis[1].barh(w_ages, w_sizes, age_group_length,
-                 align='center', color=colors[1], edgecolor='black', alpha=transparency, label='Data')
+    axis[1].barh(w_ages, w_sizes, age_group_width,
+                 align='center', color=colors[1],
+                 edgecolor='black', alpha=transparency, label='Data')
 
     # title of each sub-figure
     axis[0].set(title='\nMen')
@@ -101,49 +106,37 @@ def plot_pyramids(observed_data, simulated_data,
         sim_data_m = []
         sim_data_wa = []
         sim_data_ma = []
-        w_count = 0
-        m_count = 0
         while j < num_length:
             while k < len(simulated_data[j]):
                 if simulated_data[j][k][1] == 1:
                     sim_data_w.append(simulated_data[j][k][2] * 100)
                     sim_data_wa.append(simulated_data[j][k][0])
-                    w_count +=1
                     k = k + 1
                 else:
                     sim_data_m.append(simulated_data[j][k][2] * 100)
                     sim_data_ma.append(simulated_data[j][k][0])
-                    m_count +=1
                     k = k + 1
             k = 0
             j = j + 1
         axis[0].scatter(sim_data_w, sim_data_wa,
-                     marker='|', linewidths=5.0, s = (10*age_group_length/2)*20 , color="blue", alpha=transparency,
-                     label = 'sim_data')
+                        marker='|', linewidths=5.0,
+                        s=(10 * age_group_width / 2) * 20,
+                        color='blue', alpha=transparency,
+                        label='Model')
         axis[1].scatter(sim_data_m, sim_data_ma,
-                     marker='|', linewidths=5.0,s = (10*age_group_length/2)*20 ,
-                     color="blue", alpha=transparency,
-                     label = 'sim_data')
+                        marker='|', linewidths=5.0,
+                        s=(10 * age_group_width / 2) * 20,
+                        color='blue', alpha=transparency,
+                        label='Model')
     else:
         pass
 
-
-    # handlesx, labelsx = axis[0].get_legend_handles_labels()
-    # legend0 = axis[0].legend(handlesx, labelsx)
-    # legend0.get_texts()[0].set_text('Data_set')
-    # handlesy, labelsy = axis[1].get_legend_handles_labels()
-    # legend1 = axis[1].legend(handlesy, labelsy)
-    # legend1.get_texts()[0].set_text('Data_set')
-
-    axis[0].legend(markerscale=0.5)
-    axis[1].legend(markerscale=0.5)
+    if legend is not None:
+        axis[0].legend(markerscale=0.5)
+        axis[1].legend(markerscale=0.5)
 
     fig.tight_layout()
     # st.set_y(1)
     # fig.subplots_adjust(top=1)
     fig.show()
 
-pyrfig = plot_pyramids([[0, 0, 0.1], [0, 1, 0.2], [5, 0, 0.3], [5, 1, 0.4], [10, 0, 0.6], [10, 1, 0.4]], [[[0, 0, 0.5], [0, 1, 0.3],
-[5, 0, 0.5], [5, 1, 0.6]], [[0, 0, 0.2], [0, 1, 0.4], [5, 0, 0.9], [5, 1, 0.4]]])
-
-plt.show(pyrfig)
