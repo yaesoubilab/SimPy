4207e285e1028b6d0eac27850ec4db893b5f979e
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-06-26 16:19:34 -0400

    update param class

--- SimPy/ParameterClasses.py
+++ SimPy/ParameterClasses.py
@@ -10,10 +10,22 @@ class _Parameter:
 
 
 class Constant(_Parameter):
-    def __init__(self,  id, value):
+    def __init__(self, value, id=0):
 
         _Parameter.__init__(self, id=id)
         self.value = value
 
     def sample(self, rng=None, time=None):
         pass
+
+
+class Division(_Parameter):
+    def __init__(self, numerator, denominator, id=0):
+        _Parameter.__init__(self, id=id)
+
+        self.numerator = numerator
+        self.denominator = denominator
+        self.value = numerator.value/denominator.value
+
+    def sample(self, rng=None, time=None):
+        self.value = self.numerator.value/self.denominator.value
