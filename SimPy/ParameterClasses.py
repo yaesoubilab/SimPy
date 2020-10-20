02f6a100713cc046499ba7fed2eac8a02ddf055c
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2020-10-20 13:02:52 -0400

    params

--- SimPy/ParameterClasses.py
+++ SimPy/ParameterClasses.py
@@ -1,8 +1,9 @@
 
 class _Parameter:
     
-    def __init__(self, id):
+    def __init__(self, id=None, name=None):
         self.id = id
+        self.name = name
         self.value = None
 
     def sample(self, rng=None, time=None):
@@ -10,19 +11,29 @@ class _Parameter:
 
 
 class Constant(_Parameter):
-    def __init__(self, value, id=0):
+    def __init__(self, value, id=None, name=None):
 
-        _Parameter.__init__(self, id=id)
+        _Parameter.__init__(self, id=id, name=name)
         self.value = value
 
     def sample(self, rng=None, time=None):
         pass
 
 
-class Inverse(_Parameter):
-    def __init__(self, par, id=0):
+class BetaWithConfidenceInterval(_Parameter):
+    def __init__(self, mean, confidence_interval, confidence_level=0.05, id=None, name=None):
 
-        _Parameter.__init__(self, id=id)
+        _Parameter.__init__(self, id=id, name=name)
+        self.parameters = 0
+
+    def sample(self, rng=None, time=None):
+        pass
+
+
+class Inverse(_Parameter):
+    def __init__(self, par, id=None, name=None):
+
+        _Parameter.__init__(self, id=id, name=name)
         self.par = par
         self.value = 1 / self.par.value
 
@@ -31,9 +42,9 @@ class Inverse(_Parameter):
 
 
 class Division(_Parameter):
-    def __init__(self, par_numerator, par_denominator, id=0):
-        _Parameter.__init__(self, id=id)
+    def __init__(self, par_numerator, par_denominator, id=None, name=None):
 
+        _Parameter.__init__(self, id=id, name=name)
         self.numerator = par_numerator
         self.denominator = par_denominator
         self.sample()
@@ -43,9 +54,9 @@ class Division(_Parameter):
 
 
 class Product(_Parameter):
-    def __init__(self, parameters, id=0):
-        _Parameter.__init__(self, id=id)
+    def __init__(self, parameters, id=None, name=None):
 
+        _Parameter.__init__(self, id=id, name=name)
         self.parameters = parameters
         self.sample()
 
